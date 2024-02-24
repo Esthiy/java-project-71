65272dc20595703add1f5de6b7e840ac587d4941
Valentina Degai <valentina.o.degay@gmail.com>
2024-02-24 14:35:53 +0300

    feat: step 3, added picocli dependency

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "app", mixinStandardHelpOptions = true, version = "app 1.0",
+        description = "Calculate diff between two files")
+public class App implements Callable<Integer> {
     public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() {
         System.out.println("Hello world!");
+        return 0;
     }
 }
