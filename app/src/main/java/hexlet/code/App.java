21f83925538258922599571e0ed848febed2847a
Valentina Degai <valentina.o.degay@gmail.com>
2024-03-20 00:06:26 +0300

    feat: step 8

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -17,8 +17,9 @@ public class App implements Callable<String> {
     @Parameters(index = "1", paramLabel = "filePath2", description = "path to second file")
     private String filePath2;
 
-    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
-    private String format = "stylish";
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]",
+            defaultValue = "stylish")
+    private String format;
 
     public App(String filePath1, String filePath2, String format) {
         this.filePath1 = filePath1;
